1c92f5056148223cfce756f0cc435bef2130ef30
Nguyentungnc11 <thanhtungnguyenddk@gmail.com>
2021-01-08 11:07:01 +0700

    up

--- src/bai5_Access_Modifier/thuc_hanh/AccessModifier.java
+++ src/bai5_Access_Modifier/thuc_hanh/AccessModifier.java
@@ -1,5 +1,17 @@
 package bai5_Access_Modifier.thuc_hanh;
 
 public class AccessModifier {
-
+    public static class A {
+        public int data = 40;
+        public static void msg() {
+            System.out.println("Hello java");
+        }
+    }
+    public static class Simple {
+        public static void main(String args[]) {
+            A obj = new A();
+            System.out.println(obj.data);
+            A.msg();
+        }
+    }
 }
---
+++ src/bai5_Access_Modifier/thuc_hanh/pack/TestAccessModifier.java
@@ -0,0 +1,12 @@
+package bai5_Access_Modifier.thuc_hanh.pack;
+
+import bai5_Access_Modifier.thuc_hanh.AccessModifier;
+
+public class TestAccessModifier {
+    public static void main(String args[]) {
+
+        AccessModifier.A obj = new AccessModifier.A();
+
+        obj.msg();
+    }
+}
